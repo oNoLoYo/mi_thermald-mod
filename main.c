aaf99aeade4e7225926e9645ce509d154bc130d0
u_nuts <119465045+oNoLoYo@users.noreply.github.com>
2025-06-13 09:41:19 +0800

    Update main.c

--- main.c
+++ main.c
@@ -50,13 +50,13 @@ int main()
                     {
                         capacity_str[strcspn(capacity_str, "\n")] = 0;
                         sscanf(capacity_str, "%d", &capacity);
-                    }
+                    }                    
 
                     if (capacity <= 80)
                     {
-                        // 电量 ≤ 80%，设置电流为 7,000,000 µA（7A），约 35W（5V x 7A）
-                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 7000000);
-                    }
+                        // 电量 ≤ 80%，设置电流为 7,500,000 µA（7.5A），约 37W（5V x 7.5A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 7500000);
+                    }                   
                     else
                     {
                         // 电量 > 80%，设置电流为 4,000,000 µA（4A），约 20W（5V x 4A）
@@ -72,6 +72,38 @@ int main()
                         sscanf(capacity_str, "%d", &capacity);
                     }
 
+                    switch(capacity / 10) {
+                        case 0:
+                        // 电量 00% ~ 10%，设置电流为 12,400,000 µA（12.4A），约 62W（5V x 12.4A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 12400000);
+                        break;
+                        case 1:
+                        // 电量 10% ~ 20%，设置电流为 10,400,000 µA（7A），约 52W（5V x 10.4A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 10400000);
+                        break;
+                        case 2:
+                        // 电量 20% ~ 30%，设置电流为 9,000,000 µA（9A），约 45W（5V x 9A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 9000000);
+                        break;
+                        case 3:
+                        // 电量 30% ~ 40%，设置电流为 8,000,000 µA（8A），约 40W（5V x 8A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 8000000);
+                        break;
+                        case 4...7:
+                        // 电量 50% ~ 80%，设置电流为 7,500,000 µA（7.5A），约 37W（5V x 7.5A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 7500000);
+                        break;
+                        case 8:
+                        // 电量 80% ~ 90%，设置电流为 6,000,000 µA（6A），约 30W（5V x 6A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 6000000);
+                        break;
+                        case 9:
+                        // 电量 90% ~ 100%，设置电流为 4,000,000 µA（5A），约 20W（5V x 4A）
+                        control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 4000000);
+                        break;
+                    }
+
+                    /*
                     if (capacity <= 30)
                     {
                         // 电量 ≤ 30%，设置电流为 10,000,000 µA（10A），约 50W（5V x 10A）
@@ -89,6 +121,7 @@ int main()
                         // 电量 > 80%，设置电流为 5,000,000 µA（5A），约 25W（5V x 5A）
                         control_charge_speed("/sys/class/power_supply/battery/constant_charge_current", 5000000);
                     }
+                    */
                 }
 
                 if (charge_type_fp)
@@ -101,7 +134,7 @@ int main()
         if (fp)
             fclose(fp);
 
-        sleep(2); // 每2秒检查一次电池状态与调节需求
+        sleep(10); // 每2秒检查一次电池状态与调节需求
     }
     return 0;
 }
